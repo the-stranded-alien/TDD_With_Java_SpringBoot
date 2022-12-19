61fb2c6a69f6b1de6669e7b728d8f9901455a6dc
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-19 22:26:43 +0530

    TDD - Money Problem

---
+++ src/main/java/money/tdd/Dollar.java
@@ -0,0 +1,13 @@
+package money.tdd;
+
+public class Dollar {
+
+    int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
