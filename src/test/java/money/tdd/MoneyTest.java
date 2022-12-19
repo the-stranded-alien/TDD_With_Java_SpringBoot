070ce85518c4258f3c225587db100a8fc72ba0e0
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-19 22:43:02 +0530

    making field private

--- src/test/java/money/tdd/MoneyTest.java
+++ src/test/java/money/tdd/MoneyTest.java
@@ -8,16 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplication() {
+    void testMultiplicationDollar() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), product);
         product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(15), product);
     }
 
     @Test
-    void testEquality() {
+    void testEqualityDollar() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(8));
     }
