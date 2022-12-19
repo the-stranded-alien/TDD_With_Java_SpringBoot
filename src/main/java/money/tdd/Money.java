458fb4305d234faa12982d850d0c7ecb6ee53cb3
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-19 23:36:24 +0530

    removing direct refference and refactoring

--- src/main/java/money/tdd/Money.java
+++ src/main/java/money/tdd/Money.java
@@ -1,8 +1,17 @@
 package money.tdd;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
+    public abstract Money times(int multiplier);
+
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Franc franc(int amount) {
+        return new Franc(amount);
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
