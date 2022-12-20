3eabd9dd321840ec126f85cd7e1eb0a26fefd67f
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-20 13:21:32 +0530

    removing subclasses and refactoring tests

--- src/test/java/money/tdd/MoneyTest.java
+++ src/test/java/money/tdd/MoneyTest.java
@@ -8,30 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
-        Dollar five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
+    void testMultiplication() {
+        Money fiveD = Money.dollar(5);
+        assertEquals(Money.dollar(10), fiveD.times(2));
+        assertEquals(Money.dollar(15), fiveD.times(3));
+
+        Money fiveF = Money.franc(5);
+        assertEquals(Money.franc(10), fiveF.times(2));
+        assertEquals(Money.franc(15), fiveF.times(3));
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
-        assertNotEquals(Money.dollar(5), Money.franc(5));
-    }
 
-    @Test
-    void testMultiplicationFranc() {
-        Franc five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc() {
         assertEquals(Money.franc(5), Money.franc(5));
         assertNotEquals(Money.franc(5), Money.franc(8));
+
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
