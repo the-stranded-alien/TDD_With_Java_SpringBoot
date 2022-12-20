6d56112e4eeac2502d720cc65b300db92eb0637a
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-20 17:18:21 +0530

    clearer exchange implement and test

--- src/main/java/money/tdd/Expression.java
+++ src/main/java/money/tdd/Expression.java
@@ -2,4 +2,7 @@ package money.tdd;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+    public Expression plus(Expression addend);
+
+    Expression times(int multiplier);
 }
--- src/main/java/money/tdd/Money.java
+++ src/main/java/money/tdd/Money.java
@@ -21,7 +21,8 @@ public class Money implements Expression {
         return new Money(amount, "CHF");
     }
 
-    public Money times(int multiplier) {
+    @Override
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
     }
     public boolean equals(Object object) {
@@ -38,12 +39,13 @@ public class Money implements Expression {
                 '}';
     }
 
-    public Expression plus(Money addend) {
-        return new Sum(this, addend);
-    }
-
     @Override
     public Money reduce(Bank bank, String to) {
         return new Money(amount / bank.rate(this.currency, to), to);
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
     }
 }
